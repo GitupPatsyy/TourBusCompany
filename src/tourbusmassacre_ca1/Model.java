ccb3ca0b2a4386cb46c8a82f57e6ff1cf57d363b
GitupPatsyy <roryplunkettboyle12@gmail.com>
2015-10-21 20:30:19 +0100

    Model Class working, Hardcoded Bus there

--- src/tourbusmassacre_ca1/Model.java
+++ src/tourbusmassacre_ca1/Model.java
@@ -5,10 +5,57 @@
  */
 package tourbusmassacre_ca1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author rorypb
  */
 public class Model {
+
+    private static Model instance = null;
+
+    public static synchronized Model getInstance() {
+        if (instance == null) {
+            instance = new Model();
+        }
+        return instance;
+    }
     
-}
+    private List<Buses> buses;
+    private Model(){
+        //Array List for Buses
+        this.buses = new ArrayList<Buses>();
+        //Adding a Bus for Hardcode Test
+        this.buses.add(new Buses("07TN9999", "Mercedes", "Vito", 2.5, "14/01/2015", "22/12/2015"));
+    }
+        
+        public List<Buses> getBuses()
+        {
+            return new ArrayList<Buses>(this.buses);
+        }
+        //Method to Add Bus to List
+        public void addBus(Buses b){
+            this.buses.add(b);
+        }
+        public Buses findBusByReg(String regNum){
+            Buses b = null;
+            int i =0;
+            boolean found = false;
+            while (i < this.buses.size() && !found) {                
+                b = this.buses.get(i);
+                if (b.getRegNum() == regNum) {
+                    found = true;
+                } else {
+                    i++;
+                }
+            }
+            if(!found){
+                b = null;
+            }
+            return b;
+        }
+    }
+
+
